39a0d18e6b132e97acae1a063797f46c72a2cbe7
Enes Incekara <enesincekara61@gmail.com>
2026-06-12 16:19:45 +0300

    feat: setup spring boot 3.x with clean architecture packages

---
+++ price-pulse/core/src/test/java/com/pricepulse/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pricepulse.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
